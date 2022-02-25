e48bac287c9e60296f3389da41b02242eb14514b
Manish8951 <manish.xworkz@gmail.com>
2022-02-25 22:12:20 +0530

    data types

---
+++ datatypes/datatype.java
@@ -0,0 +1,31 @@
+class lenovo{
+        public static void main(String []ar){
+        
+         byte noOfLags =99;
+         System.out.println(noOfLags);
+
+         short value = 15000;
+         System.out.println(value);
+  
+         int adobe = 987652;
+         System.out.println(adobe);
+
+         long mobile = 89516574123456l;
+         System.out.println(mobile);
+   
+         float number= 0.18618f;
+         System.out.println(number);
+
+         double number2 =8.123456;
+         System.out.println(number2);
+
+         char present ='Y';
+         System.out.println(present);
+
+         boolean num= false;
+         System.out.println(num);
+       }
+}
+
+         
+        
